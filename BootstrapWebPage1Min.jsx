b9bf002254932a2c1633ec4deed7008db28f7e7b
Poorvi Agarwal <poorviagarwal.cse22@jecrc.ac.in>
2021-04-22 00:00:41 +0530

    Creating basic website using bootstrap

---
+++ BootstrapWebPage1Min.jsx
@@ -0,0 +1,49 @@
+
+
+
+                                                //vdeo 49
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+const App=()=>{
+    return(
+        <>
+        <h1 className="text-center text-danger text-capitalize">Welcome</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-sm">
+                     <div class="card">
+  <img src="https://picsum.photos/200/300" class="card-img-top" alt="..." height="170px"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                </div>
+                <div className="col-sm">
+                    <div class="card" >
+  <img src="https://picsum.photos/201/300" class="card-img-top"  height="170px"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                </div>
+                <div className="col-sm">
+                     <div class="card">
+  <img src="https://picsum.photos/202/300" class="card-img-top"  height="170px"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                </div>
+             </div>
+        </div>
+        </>
+    );
+
+};
+export default App;
