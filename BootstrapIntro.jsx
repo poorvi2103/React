33f8640cd1cf3a3063548eded5d881b0a2748a63
Poorvi Agarwal <poorviagarwal.cse22@jecrc.ac.in>
2021-04-21 23:59:57 +0530

    code for basic intro of Bootstrap

---
+++ BootstrapIntro.jsx
@@ -0,0 +1,15 @@
+
+                                                                //vdo 47
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+const App=()=>{
+    return(
+        <>
+        <h1>Welcome
+        </h1>
+        <button className="btn btn-success">Click Me 😄</button>
+        </>
+    );
+
+};
+export default App;
