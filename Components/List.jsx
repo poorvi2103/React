09435c8795cc04b20d69c4598f239f6638dea336
Poorvi Agarwal <poorviagarwal.cse22@jecrc.ac.in>
2021-04-22 00:01:41 +0530

    Was learning bout components

---
+++ Components/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function List()
+{
+    return (
+    <ol>
+        <li>Watermelon</li>
+        <li>Mango</li>
+    </ol>
+    );
+}
+export default List;
---
+++ Components/Paragraph.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+function Paragraph()
+{
+    return <p>My favourite fruits are:</p>;
+}
+export default Paragraph;
